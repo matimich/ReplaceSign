94e172c368665198517e5e5b042c94beb09b3cbe
matimich <matimich@vp.pl>
2018-09-11 00:07:47 +0200

    ok

--- ReplaceSign.c
+++ ReplaceSign.c
@@ -9,7 +9,7 @@ void ReplaceSign(void)
     char sign;      //remove
     char sign_2;    //new
 
-    printf("Enter  new-old sign:");
+    printf("\n Enter a new/old sign:\n");     //inputting a new and an old sign
     scanf("%c-%c",&sign_2,&sign);
 
 
@@ -21,38 +21,37 @@ void ReplaceSign(void)
     char *point;
 
 
-    if((file = fopen (OLD_FILE_PATH,"r"))==NULL)       //opening core file
+    if((file = fopen (OLD_FILE_PATH,"r"))==NULL)       //opening a core file
     {
         printf ("Error, please try again");
         exit(EXIT_FAILURE);
     }
 
-    if((another = fopen (NEW_FILE_PATH,"w"))==NULL)    //opening file where changed tekst is going to be saved
+    if((another = fopen (NEW_FILE_PATH,"w"))==NULL)    //opening file where a changed tekst is going to be saved
     {
         printf ("Error, please try again");
         exit(EXIT_FAILURE);
     }
 
-    while(fgets(array,100,file) != NULL) //changing signs
+    while(fgets(array,100,file) != NULL) //extracting data from a file
     {
 
-
-        if (strchr(array,sign) != NULL)
+        if (strchr(array,sign) != NULL) //looking for common signs
         {
             do
             {
-                point = strchr(array,sign);
+                point = strchr(array,sign); //changing signs
                 *point = sign_2;
 
 
             }while(strchr(array,sign) != NULL);
 
-            fputs(array,another);
+            fputs(array,another);   //saving changed strings
 
         }
         else
-        {   fputs(array,another);
-
+        {
+            fputs(array,another);   //saving unchanged strings
         }
 
     }
--- ReplaceSign.h
+++ ReplaceSign.h
@@ -2,6 +2,8 @@
 #define _CHANGE_
 
 
+#define OLD_FILE_PATH "text.txt"        //a name of the file with a text to edit
+#define NEW_FILE_PATH "new.txt"         // a name of the file with a changed text
 
 void ReplaceSign(void);
 
